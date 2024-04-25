fa8e1f21bcbe51bb6ec8b0193982eb58265b2aaf
Nouf <nalknedri@stu.kau.edu.sa>
2024-04-25 18:27:27 +0300

    Initial Commit

---
+++ Categories/CategoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CategoriesApp.swift
+//  Categories
+//
+//  Created by Nouf Faisal  on 16/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct CategoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
